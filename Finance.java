86fdf5df56f3e112f3be47aa6dcc9a8efecf797d
rp246c6 <91663071+rp246c6@users.noreply.github.com>
2022-12-02 12:24:58 +0000

    1-Added-Commission

--- Finance.java
+++ Finance.java
@@ -2,6 +2,22 @@ public class Finance
 {
   public static void main(String args[])
   {
+    int commission = 10;
     System.out.println("Welcome to Finance");
+    System.out.println("checking commission..");
+    Finance com = new Finance();
+    if(com.checkCommission(commission))
+    {
+      System.out.println("Eligible for commission");
+    }
+    else
+    {
+      System.out.println("Not Eligible for commission");
+    }
+
+  }
+  public Boolean checkCommission(int commission)
+  {
+    return commission>100?true:false;
   }
 }
